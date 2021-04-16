8862682ff6c15a85aa118afede0e7eb58ce511fd
devonriley <devontriley@gmail.com>
2021-04-16 14:27:05 -0400

    Hide similar movies when none found

--- src/components/MovieSearch/MovieListEntryDetails.js
+++ src/components/MovieSearch/MovieListEntryDetails.js
@@ -12,15 +12,20 @@ const MovieListEntryDetails = ({detailsExpanded, movie, similarMovies}) =>
                 <img src={'https://image.tmdb.org/t/p/original/'+movie.poster_path} />
                 <div className="px-4 pb-4">
                     <p>{movie.overview}</p>
-                    <hr />
-                    <h5>Similar Movies</h5>
-                    <div className="similar-movies row">
-                        {similarMovies.filter((m, index) => index < 6).map(m =>
-                            <div key={m.id} className="col-auto">
-                                <img src={'https://image.tmdb.org/t/p/original/'+m.poster_path} width="100px" />
+
+                    {similarMovies.length > 0 &&
+                            <div>
+                                <hr />
+                                <h5>Similar Movies</h5>
+                                <div className="similar-movies row">
+                                    {similarMovies.filter((m, index) => index < 6).map(m =>
+                                        <div key={m.id} className="col-auto">
+                                            <img src={'https://image.tmdb.org/t/p/original/'+m.poster_path} width="100px" />
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        )}
-                    </div>
+                    }
                 </div>
             </div>
         </div>
