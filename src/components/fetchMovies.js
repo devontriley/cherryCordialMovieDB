5dd09bdd921db1ae70b0d3bb2347e97fec85bb21
devonriley <devontriley@gmail.com>
2021-04-16 11:31:56 -0400

    Sort fetchMovies by DESC release_date

--- src/components/fetchMovies.js
+++ src/components/fetchMovies.js
@@ -2,9 +2,17 @@ import axios from 'axios';
 
 const fetchMovies = (searchTerm) =>
 {
-    return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=b3a999b7703140535e95baeff2e338fa&query=${searchTerm}&sort`)
+    return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=b3a999b7703140535e95baeff2e338fa&query=${searchTerm}`)
         .then(res => {
-            return res.data.results;
+            let results = res.data.results.sort((a,b) =>
+                {
+                    let dateA = new Date(a.release_date);
+                    let dateB = new Date(b.release_date);
+
+                    return dateB.getTime() - dateA.getTime()
+                });
+
+            return results;
         });
 }
 
