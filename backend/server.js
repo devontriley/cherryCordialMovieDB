da631ded2f80823c31849d9bfcec878a1ec1d912
devonriley <devontriley@gmail.com>
2021-04-22 13:11:44 -0400

    adding production conditional to serve build path to the client

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ const users = require('./routes/users');
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static(path.join(__dirname, '/client/build')))
+    app.get('*', (req,res) =>
+    {
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+    })
+}
+
 app.use(cors());
 
 // Bodyparser middleware
