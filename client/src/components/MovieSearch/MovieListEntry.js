1050e91325d1167513ffff0ed701f47afb9e581f
devonriley <devontriley@gmail.com>
2021-04-26 11:16:56 -0400

    Added plus/minus icons for add/remove to watchlist

--- client/src/components/MovieSearch/MovieListEntry.js
+++ client/src/components/MovieSearch/MovieListEntry.js
@@ -15,6 +15,14 @@ const MovieListEntry = ({movie, addMovieToWatchlist, setModalMovie, currentUser,
         setDetailsExpanded(!detailsExpanded);
     }
 
+    const handleWatchlistClick = () =>
+    {
+        // If watchlist already contains movie, return
+        if(currentUser.watchlist.some(ele => ele.id === movie.id)) return;
+
+        addMovieToWatchlist(movie, currentUser.email, setErrors);
+    }
+
     useEffect(() =>
     {
         fetchSimilarMovies(movie)
@@ -27,7 +35,20 @@ const MovieListEntry = ({movie, addMovieToWatchlist, setModalMovie, currentUser,
         <li className="list-group-item" key={movie.id}>
             <div className="d-flex justify-content-between">
                 <button className="btn btn-link mb-0" onClick={handleClick}>{movie.title + ' (' + date + ')'}</button>
-                <button className="add-to-watchlist btn btn-link" onClick={() => {addMovieToWatchlist(movie, currentUser.email, setErrors)}}>+ Watchlist</button>
+                <button
+                    className={`add-to-watchlist btn btn-link`}
+                    onClick={handleWatchlistClick}>
+                    { currentUser.watchlist.some(ele => ele.id === movie.id)
+                        ?
+                            <svg style={{fill: 'red'}} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="minus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="15" className="svg-inline--fa fa-minus fa-w-14 fa-3x">
+                                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                            </svg>
+                        :
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="15" className="svg-inline--fa fa-plus fa-w-14 fa-3x">
+                                <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                            </svg>
+                    }
+                </button>
             </div>
             <MovieListEntryDetails detailsExpanded={detailsExpanded} movie={movie} similarMovies={similarMovies} setModalMovie={setModalMovie} />
         </li>
